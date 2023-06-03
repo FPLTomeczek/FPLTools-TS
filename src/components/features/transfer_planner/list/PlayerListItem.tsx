a2a931f24f1e7790eaf1904389138687385d66b4
FPLTomeczek <tomaszs18m@gmail.com>
2023-06-03 22:51:43 +0200

    AddPlayerFromList

--- src/components/features/transfer_planner/list/PlayerListItem.tsx
+++ src/components/features/transfer_planner/list/PlayerListItem.tsx
@@ -1,56 +1,10 @@
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import { teamsList } from "./data";
-import { addPick } from "../../../../features/managerTeam/managerTeamSlice";
 import { Player } from "../interfaces/players";
-import { useAppSelector, useAppDispatch } from "../../../../app/hooks";
+import AddPlayerToTeamButton from "./AddPlayerToTeamButton";
 
 const PlayerListItem = ({ player }: { player: Player }) => {
-  const managerPicks = useAppSelector((state) => state.managerTeam.picks);
-
-  const availablePositions = [
-    ...new Set(
-      managerPicks
-        .filter((pick) => pick.web_name === "Blank")
-        .map((pick) => pick.element_type)
-    ),
-  ];
-
-  const foundTeam = teamsList.find((team) => team.value === player.team);
-
-  const color = foundTeam ? foundTeam.color : "#000000";
-
-  const dispatch = useAppDispatch();
-
-  const addPlayerToTeam = () => {
-    dispatch(addPick(player));
-  };
-
   return (
     <div key={player.id} className="player-list-item">
-      <div className="player-add-button-color">
-        {managerPicks.find((pick) => pick.id === player.id) ||
-        !availablePositions.includes(player.element_type) ? (
-          <button style={{ display: "flex", alignItems: "center" }} disabled>
-            <AddCircleIcon color="disabled" />
-          </button>
-        ) : (
-          <button
-            style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-            onClick={addPlayerToTeam}
-          >
-            <AddCircleIcon color="success" />
-          </button>
-        )}
-        <div
-          className="team-color"
-          style={{
-            background: Array.isArray(color)
-              ? `linear-gradient(to right, ${color[0]} 0%, ${color[0]} 50%, ${color[1]} 50%, ${color[1]} 100%)`
-              : color,
-          }}
-        ></div>
-      </div>
-
+      <AddPlayerToTeamButton player={player} />
       <p className="player-list-name">{player.web_name}</p>
       <p className="player-list-number" data-testid="player-team-item">
         {player.team}
