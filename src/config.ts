e344d5021e4db93e76d4f8dc5bbdf59e419fae73
FPLTomeczek <tomaszs18m@gmail.com>
2023-06-07 10:36:51 +0200

    url_update

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const dev = {
 };
 
 const prod = {
-  url: "https://fpltools-api.onrender.com/api/",
+  url: "https://fpltools-api.onrender.com/api",
 };
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
