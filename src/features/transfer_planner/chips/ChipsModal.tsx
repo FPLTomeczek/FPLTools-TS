057bce8a86fd8e091b16fc17413f22dadee8c916
FPLTomeczek <tomaszs18m@gmail.com>
2023-07-08 21:06:52 +0200

    feat: chips modal background interaction

--- src/features/transfer_planner/chips/ChipsModal.tsx
+++ src/features/transfer_planner/chips/ChipsModal.tsx
@@ -13,16 +13,26 @@ const ChipsModal = ({
 
   return (
     <ChipsModalStyled isModalOpen={isModalOpen}>
-      <p>Do you want to cancel your chip?</p>
-      <button
-        onClick={() => {
-          dispatch(setChip({ chipName: "" }));
-          setIsModalOpen(false);
-        }}
-      >
-        Yes
-      </button>
-      <button onClick={() => setIsModalOpen(false)}>No</button>
+      <div className="modal-inside">
+        <p>
+          Do you want to remove your current chip and delete transfer planner
+          state from current gameweek onwards?
+        </p>
+        <div className="chips-modal-btns">
+          <button
+            className="btn-delete"
+            onClick={() => {
+              dispatch(setChip({ chipName: "" }));
+              setIsModalOpen(false);
+            }}
+          >
+            Delete
+          </button>
+          <button className="btn-primary" onClick={() => setIsModalOpen(false)}>
+            Cancel
+          </button>
+        </div>
+      </div>
     </ChipsModalStyled>
   );
 };
@@ -30,10 +40,25 @@ const ChipsModal = ({
 const ChipsModalStyled = styled.div<{ isModalOpen: boolean }>`
   display: ${(props) => (props.isModalOpen ? "block" : "none")};
   position: fixed;
-  bottom: 500px;
-  left: 600px;
-  width: 200px;
-  margin: 0 auto;
+  bottom: 0;
+  left: 0;
+  top: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  .modal-inside {
+    background-color: var(--primary-color);
+    border: 4px solid var(--secondary-color);
+    border-radius: var(--primary-border-radius);
+    margin: 0 auto;
+    margin-top: 20vh;
+    padding: 1rem;
+    width: 200px;
+  }
+  .chips-modal-btns {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
 `;
 
 export default ChipsModal;
--- src/features/transfer_planner/pitch/Bench.tsx
+++ src/features/transfer_planner/pitch/Bench.tsx
@@ -2,14 +2,13 @@ import Pick from "./Pick";
 import { Pick as IPick } from "../interfaces/drafts";
 import { BenchStyled } from "./Pitch.styled";
 import { FIRST_ELEVEN_PLAYERS } from "../../../constants";
-import { useDraft } from "../../../app/customHooks";
+import { useThisGameweekData } from "../../../app/customHooks";
 import { Chip } from "../chips/chipsEnums";
 
 const Bench = ({ picks }: { picks: IPick[] }) => {
   let playerStartIndex = FIRST_ELEVEN_PLAYERS;
-  const gameweek = useDraft().gameweek;
   const currentChipIsBB =
-    useDraft().dataByGameweeks[gameweek]?.chipByGameweek === Chip.BENCH_BOOST;
+    useThisGameweekData()?.chipByGameweek === Chip.BENCH_BOOST;
 
   if (picks.length === 0) {
     return null;
