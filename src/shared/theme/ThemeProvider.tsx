3624e326618b1c8ab2b239d624fe3ec96e9fa233
FPLTomeczek <tomaszs18m@gmail.com>
2023-08-23 14:20:24 +0200

    fix: useState unused import

--- src/shared/theme/ThemeProvider.tsx
+++ src/shared/theme/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext } from "react";
 import { ThemeProvider as DefaultThemeProvider } from "styled-components";
 import { useLocalStorage } from "usehooks-ts";
 import { isEqual } from "lodash";
